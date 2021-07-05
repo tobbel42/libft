b0c4adcbf887526c4097b6e7969c7aaa12aed593
Tobias Grossmann <tgrossma@3-G-4.42heilbronn.de>
2021-07-05 17:04:10 +0200

    added bonus fcts and updated makefile with bonus rule

---
+++ ft_memccpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tgrossma <tgrossma@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/28 13:57:40 by tgrossma          #+#    #+#             */
+/*   Updated: 2021/06/28 13:57:40 by tgrossma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+	size_t		i;
+	char		*ptr_dest;
+	char		*ptr_src;
+
+	ptr_dest = (char *)dest;
+	ptr_src = (char *)src;
+	i = 0;
+	while (i < n)
+	{
+		ptr_dest[i] = ptr_src[i];
+		if ((unsigned char)ptr_src[i] == (unsigned char)c)
+			return (dest + i + 1);
+		i++;
+	}
+	return (0);
+}
